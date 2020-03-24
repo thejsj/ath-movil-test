deb46702f0d271ce98cf42d968dfda6bbc850400
thejsj <jorge.silva@thejsj.com>
2020-03-23 18:11:17 -0700

    Remove console.log

--- athmovil/ATHMPayment.js
+++ athmovil/ATHMPayment.js
@@ -1,7 +1,3 @@
 import { NativeModules } from 'react-native';
 
-console.log('NativeModules.ATHMovil')
-console.log(NativeModules)
-console.log(typeof NativeModules)
-
 module.exports = NativeModules.ATHMovil;
